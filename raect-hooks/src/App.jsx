4ff73d0f9b0b2ec942d92191695c5656e641ad44
lucky45789 <priyankalima28@gmail.com>
2024-05-30 18:35:18 +0530

    new projectfile added

---
+++ raect-hooks/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Counter from "./components/usestate/counter/Counter.jsx";
+
+function App() {
+  return (
+    <div>
+      <h1>Learing React Hooks:</h1>
+      <Counter/>
+  
+    </div>
+  );
+}
+
+export default App;
